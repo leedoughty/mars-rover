21d658e7a42534b961b2ae5a7cec239fa411b0cf
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-11 12:09:06 +0000

    Code review amendments

--- app.js
+++ app.js
@@ -3,11 +3,6 @@ const app = express();
 const port = 3000;
 
 app.use(express.static("public"));
-app.use("/", express.static("public/index.html"));
-
-app.get("/", (request, response) => {
-  response.send("Hello World!");
-});
 
 app.listen(port, () => {
   console.log(`App listening on port:${port}`);
