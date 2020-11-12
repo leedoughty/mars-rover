fff366f99c422d3bf99f86f337075dbf0871e8c8
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-12 16:27:10 +0000

    Display dates for the Kennedy Space Center on homepage

--- public/script.js
+++ public/script.js
@@ -1,5 +1,44 @@
-const earthDateParagraph = document.querySelector("#earth-date");
 const earthDate = new Date();
-const dateText = document.createTextNode(earthDate.toLocaleString());
 
-earthDateParagraph.appendChild(dateText);
+function displayCurrentEarthDate() {
+  const earthDateParagraph = document.querySelector("#earth-date");
+  const dateText = document.createTextNode(earthDate.toLocaleString());
+
+  earthDateParagraph.appendChild(dateText);
+}
+
+displayCurrentEarthDate();
+
+function displayKennedySpaceCenterTime() {
+  const kennedyTimeParagraph = document.querySelector("#kennedy-time");
+  const kennedyTime = earthDate.toLocaleTimeString("en-US", {
+    timeZone: "America/New_York",
+  });
+  const kennedyTimeText = document.createTextNode(kennedyTime.split(" ")[0]);
+
+  kennedyTimeParagraph.appendChild(kennedyTimeText);
+}
+
+displayKennedySpaceCenterTime();
+
+function displayOffsetTimeToKennedySpaceCenter() {
+  const kennedyOffsetTime = document.querySelector("#kennedy-offset");
+  const utcOffsetTimeInHours = earthDate.getTimezoneOffset() / 60;
+  const timeBetweenKennedyAndUtc = utcOffsetTimeInHours + 5;
+  const kennedyOffset = document.createTextNode(timeBetweenKennedyAndUtc);
+  kennedyOffsetTime.appendChild(kennedyOffset);
+  const aheadOfKennedy = document.createTextNode(
+    " hours ahead of Kennedy Space Center"
+  );
+  const behindKennedy = document.createTextNode(
+    " hours ahead of Kennedy Space Center"
+  );
+
+  if (timeBetweenKennedyAndUtc > 0) {
+    kennedyOffsetTime.appendChild(aheadOfKennedy);
+  } else {
+    kennedyOffsetTime.appendChild(behindKennedy);
+  }
+}
+
+displayOffsetTimeToKennedySpaceCenter();
