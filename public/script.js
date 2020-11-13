0e6ab50787e14e0a0fbc45ceb4c7aee7358b303b
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-13 10:43:12 +0000

    MARS-9: Code review amendments - clean up Kennedy Space Center functions

--- public/script.js
+++ public/script.js
@@ -1,43 +1,37 @@
 const earthDate = new Date();
 
 function displayCurrentEarthDate() {
-  const earthDateParagraph = document.querySelector("#earth-date");
   const dateText = document.createTextNode(earthDate.toLocaleString());
-
-  earthDateParagraph.appendChild(dateText);
+  document.querySelector("#earth-date").appendChild(dateText);
 }
 
 displayCurrentEarthDate();
 
 function displayKennedySpaceCenterTime() {
-  const kennedyTimeParagraph = document.querySelector("#kennedy-time");
   const kennedyTime = earthDate.toLocaleTimeString(undefined, {
     timeZone: "America/New_York",
   });
   const kennedyTimeText = document.createTextNode(kennedyTime);
 
-  kennedyTimeParagraph.appendChild(kennedyTimeText);
+  document.querySelector("#kennedy-time").appendChild(kennedyTimeText);
 }
 
 displayKennedySpaceCenterTime();
 
 function displayOffsetTimeToKennedySpaceCenter() {
-  const kennedyOffsetTime = document.querySelector("#kennedy-offset");
   const utcOffsetTimeInHours = earthDate.getTimezoneOffset() / 60;
   const timeBetweenKennedyAndUtc = utcOffsetTimeInHours + 5;
-  const kennedyOffset = document.createTextNode(timeBetweenKennedyAndUtc);
-  kennedyOffsetTime.appendChild(kennedyOffset);
   const aheadOfKennedy = document.createTextNode(
-    " hours ahead of Kennedy Space Center)"
+    `${timeBetweenKennedyAndUtc} hours ahead of Kennedy Space Center)`
   );
   const behindKennedy = document.createTextNode(
-    " hours behind of Kennedy Space Center)"
+    `${timeBetweenKennedyAndUtc} hours behind of Kennedy Space Center)`
   );
 
   if (timeBetweenKennedyAndUtc > 0) {
-    kennedyOffsetTime.appendChild(aheadOfKennedy);
+    document.querySelector("#kennedy-offset").appendChild(aheadOfKennedy);
   } else {
-    kennedyOffsetTime.appendChild(behindKennedy);
+    document.querySelector("#kennedy-offset").appendChild(behindKennedy);
   }
 }
 
