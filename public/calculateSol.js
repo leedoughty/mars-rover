98c699a43eb3c4a68477568fa1364260b1b0782f
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-16 16:31:30 +0000

    MARS-14: Add mars sol

---
+++ public/calculateSol.js
@@ -0,0 +1,18 @@
+const earthDayInMilliseconds = 24 * 60 * 60 * 1000;
+const solDayInMilliseconds =
+  earthDayInMilliseconds + 39 * 60 * 1000 + 35.244 * 1000;
+const convertSol = solDayInMilliseconds / earthDayInMilliseconds;
+const marsRoverLandingDate = new Date(2012, 7, 6, 5, 17, 57);
+const timeSinceLandingInMilliseconds = Date.now() - marsRoverLandingDate;
+const earthDaysSinceMarsRoverLanded =
+  timeSinceLandingInMilliseconds / earthDayInMilliseconds;
+
+function calculateSol() {
+  const marsSols = Math.floor(earthDaysSinceMarsRoverLanded / convertSol);
+
+  document
+    .querySelector("#mars-sol")
+    .append(`Current Mars Sols of the Mars Rover: ${marsSols}`);
+}
+
+calculateSol();
