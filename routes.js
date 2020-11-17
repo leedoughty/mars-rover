a520341b0f9692d3f1e07572161c9be68f52f39c
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-17 13:22:36 +0000

    MARS-16: Setup /rover endpoint

--- routes.js
+++ routes.js
@@ -8,4 +8,8 @@ router.get("/photo", (request, response) => {
   response.send("Photo of the day");
 });
 
+router.get("/rover", (request, response) => {
+  response.send("Mars rover");
+});
+
 module.exports = router;
