f5e3d521cf59d89f99faa20d29ba73750eb2c8f1
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-24 13:46:53 +0000

    MARS-10: Refactor getPhotoOfTheDay middleware

--- middleware/getPhotoOfTheDay.js
+++ middleware/getPhotoOfTheDay.js
@@ -1,19 +1,13 @@
 const fetch = require("node-fetch");
 const API_KEY = require("../apiKey");
-
 const BASE_URL = "https://api.nasa.gov/planetary/apod";
 
 const getPhotoOfTheDay = async (request, response, next) => {
   const url = `${BASE_URL}?api_key=${API_KEY}`;
+  const apodResponse = await fetch(url);
+  const apodJson = await apodResponse.json();
 
-  const apodResponse = await fetch(url).then((response) =>
-    response.json().catch((error) => {
-      console.log(error);
-    })
-  );
-
-  response.json(apodResponse);
-  next();
+  response.json(apodJson);
 };
 
 module.exports = { getPhotoOfTheDay };
