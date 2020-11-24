c57b14b62a151b603c6a2e3b923b970c01ce4458
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-24 13:38:20 +0000

    MARS-10: Display photo of the day from NASA api on homepage

--- public/displayPhoto.js
+++ public/displayPhoto.js
@@ -1,13 +1,8 @@
 function displayPhoto() {
   fetch("http://localhost:3000/photo")
-    .then((response) => {
-      console.log(response);
-      console.log("in the html");
-      return response.json();
-    })
+    .then((response) => response.json())
     .then((data) => {
-      console.log(data);
+      const photoOfTheDay = document.getElementById("photo-of-the-day");
+      photoOfTheDay.setAttribute("src", `${data.url}`);
     });
 }
-
-displayPhoto();
