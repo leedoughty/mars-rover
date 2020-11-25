01f1ec2c58c4e06a9201741249aedacd1a0b41bc
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-25 16:29:00 +0000

    MARS-10: Add alt text to photo of the day

--- public/displayPhoto.js
+++ public/displayPhoto.js
@@ -5,6 +5,7 @@ function displayPhoto() {
       const photoOfTheDay = document.getElementById("photo-of-the-day");
       const photoExplanation = document.getElementById("photo-explanation");
       photoOfTheDay.setAttribute("src", `${data.url}`);
+      photoOfTheDay.setAttribute("alt", `${data.title}`);
       photoExplanation.textContent = data.explanation;
     });
 }
