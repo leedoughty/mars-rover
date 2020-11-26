f087f7d5b37f7eec564d1ec7e3c22511ce80d8df
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-26 14:37:58 +0000

    MARS-17: Setup authenticate endpoint

--- routes.js
+++ routes.js
@@ -8,6 +8,10 @@ router.use(express.urlencoded({ extended: true }));
 
 router.use("/login", express.static("public/login.html"));
 
+router.post("/authenticate", (request, response) => {
+  response.send(request.body);
+});
+
 router.get("/photo", getPhotoOfTheDay);
 
 router.use("/rover", express.static("public/rover.html"));
