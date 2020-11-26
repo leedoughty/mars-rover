b09b44e9b95bac44be1548469f8990acb7979911
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-26 14:08:48 +0000

    MARS-10: Setup error handling

--- middleware/getPhotoOfTheDay.js
+++ middleware/getPhotoOfTheDay.js
@@ -2,14 +2,22 @@ const fetch = require("node-fetch");
 const API_KEY = require("../apiKey");
 const BASE_URL = "https://api.nasa.gov/planetary/apod";
 
+const checkStatus = (response) => {
+  if (response.ok) {
+    return response;
+  } else {
+    throw new Error("error");
+  }
+};
+
 const getPhotoOfTheDay = async (request, response, next) => {
   const url = `${BASE_URL}?api_key=${API_KEY}`;
   try {
-    const apodResponse = await fetch(url);
+    const apodResponse = await fetch(url).then(checkStatus);
     const apodJson = await apodResponse.json();
     response.json(apodJson);
   } catch (error) {
-    console.log(error);
+    return response.status(500).json({ error: error.toString() });
   }
 };
 
--- public/displayPhoto.js
+++ public/displayPhoto.js
@@ -1,5 +1,14 @@
+function checkStatus(response) {
+  if (response.ok) {
+    return response;
+  } else {
+    throw new Error();
+  }
+}
+
 function displayPhoto() {
   fetch("/photo")
+    .then(checkStatus)
     .then((response) => response.json())
     .then((data) => {
       const photoOfTheDay = document.getElementById("photo-of-the-day");
@@ -7,5 +16,10 @@ function displayPhoto() {
       photoOfTheDay.setAttribute("src", `${data.url}`);
       photoOfTheDay.setAttribute("alt", `${data.title}`);
       photoExplanation.textContent = data.explanation;
+    })
+    .catch(() => {
+      const photoOfTheDay = document.getElementById("photo-of-the-day");
+      photoOfTheDay.setAttribute("src", "mars-rover.jpg");
+      photoOfTheDay.setAttribute("alt", "The Mars Rover");
     });
 }
