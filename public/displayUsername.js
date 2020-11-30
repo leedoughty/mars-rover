9c4c084c109e3e4bfa89c39bed8e4b96bbe8a1ef
Lee Doughty <leedoughtydesign@gmail.com>
2020-11-30 11:55:43 +0000

    MARS-19: Display username on homepage

---
+++ public/displayUsername.js
@@ -0,0 +1,11 @@
+function displayUsername() {
+  fetch("/username")
+    .then((response) => {
+      return response.json();
+    })
+    .then((data) => {
+      console.log(data);
+      const username = document.getElementById("account-username");
+      username.textContent = data.username;
+    });
+}
