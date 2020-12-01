9a6673d3c80e16abbcc7331bda6457683e9f00f3
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-01 15:09:49 +0000

    MARS-20: Setup register endpoint

--- routes.js
+++ routes.js
@@ -10,8 +10,13 @@ router.use(express.json());
 router.use(express.urlencoded({ extended: true }));
 
 router.use("/login", express.static("public/login.html"));
+router.use("/register", express.static("public/register.html"));
 
 router.post("/authenticate", checkUserCredentials, setUserJwt);
+
+router.post("/register", (request, response) => {
+  response.send("User registered");
+});
 
 router.get("/username", getUsername);
 
