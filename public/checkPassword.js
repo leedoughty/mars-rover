15a029ea0f0cb9d67c4cc792b6cce36174221c7d
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-01 16:21:03 +0000

    MARS-20: Add check to make sure password contains a number

--- public/checkPassword.js
+++ public/checkPassword.js
@@ -1,8 +1,14 @@
 const form = document.forms["register"];
 const password = form["password"];
 const warningMessage = document.getElementById("register-warning-message");
+
 const passwordLessThanSixCharacters = document.createElement("p");
 warningMessage.appendChild(passwordLessThanSixCharacters);
+
+const passwordContainsNoNumber = document.createElement("p");
+warningMessage.appendChild(passwordContainsNoNumber);
+
+const doesNotContainNumber = /(\d)/;
 
 form.addEventListener("input", (event) => {
   if (event.target.value.length < 6) {
@@ -11,10 +17,21 @@ form.addEventListener("input", (event) => {
   } else {
     passwordLessThanSixCharacters.textContent = "Password length: ✅";
   }
+
+  if (!doesNotContainNumber.test(event.target.value)) {
+    passwordContainsNoNumber.textContent =
+      "Your password needs to contain a number";
+  } else {
+    passwordContainsNoNumber.textContent = "Password contains a number ✅";
+  }
 });
 
 form.addEventListener("submit", (event) => {
   if (password.value.length < 6) {
+    event.preventDefault();
+  }
+
+  if (!doesNotContainNumber.test(password.value)) {
     event.preventDefault();
   }
 });
