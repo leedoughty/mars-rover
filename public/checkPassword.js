692afb388bded15dccfff81d71e92860d4e37729
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-01 15:54:45 +0000

    MARS-20: Add check for password length

---
+++ public/checkPassword.js
@@ -0,0 +1,21 @@
+const form = document.forms["register"];
+const password = form["password"];
+const warningMessage = document.getElementById("register-warning-message");
+const passwordLessThanSixCharacters = document.createElement("p");
+warningMessage.appendChild(passwordLessThanSixCharacters);
+
+form.addEventListener("input", (event) => {
+  if (event.target.value.length < 6) {
+    passwordLessThanSixCharacters.textContent =
+      "Your password is less than 6 characters long!";
+  } else {
+    passwordLessThanSixCharacters.textContent = "Password length: ✅";
+  }
+});
+
+form.addEventListener("submit", (event) => {
+  console.log(password.value.length);
+  if (password.value.length < 6) {
+    event.preventDefault();
+  }
+});
