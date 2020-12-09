105a63c39331977b87ffe12be6d847a83d49e93a
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-09 16:14:14 +0000

    MARS-21: Code review amendment - Add video explanation

--- app/homepage/displayPhoto.js
+++ app/homepage/displayPhoto.js
@@ -13,9 +13,11 @@ function displayPhoto() {
     .then((data) => {
       if (data.media_type === "video") {
         const videoOfTheDay = document.getElementById("video-of-the-day");
+        const photoExplanation = document.getElementById("photo-explanation");
         videoOfTheDay.setAttribute("style", "display: block");
         videoOfTheDay.setAttribute("src", `${data.url}`);
         videoOfTheDay.setAttribute("alt", `${data.title}`);
+        photoExplanation.textContent = data.explanation;
       } else {
         const photoOfTheDay = document.getElementById("photo-of-the-day");
         const photoExplanation = document.getElementById("photo-explanation");
