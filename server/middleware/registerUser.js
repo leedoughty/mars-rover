097b48d43dcc0ea344b314de5429f39be13802c5
Lee Doughty <leedoughtydesign@gmail.com>
2020-12-14 15:17:20 +0000

    MARS-28: Hash users password when registering

--- server/middleware/registerUser.js
+++ server/middleware/registerUser.js
@@ -1,10 +1,24 @@
 const { Users } = require("../database/databaseModel");
+const bcrypt = require("bcrypt");
 
 const registerUser = (request, response, next) => {
   const { username, password, email } = request.body;
 
   Users.create({ username, password, email })
     .then(() => {
+      const saltRounds = 10;
+
+      bcrypt.hash(password, saltRounds, (error, hash) => {
+        Users.update(
+          { password: hash },
+          {
+            where: {
+              username: username,
+            },
+          }
+        );
+      });
+
       return next();
     })
     .catch((error) => {
